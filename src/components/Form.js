b17b9d0936e3f3a903a8b3d893b8c9f962db6b32
zuleika <njokizuleika@gmail.com>
2023-05-31 14:53:44 +0300

    done forms submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -11,13 +12,55 @@ function Form(props) {
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = { firstName: firstName, lastName: lastName };
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
+    setFirstName("");
+    setLastName("");
+  }
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+  const [errors, setErrors] = useState([]);
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    // first name is required
+    if (firstName.length > 0) {
+      const formData = { firstName: firstName, lastName: lastName };
+      const dataArray = [...submittedData, formData];
+      setSubmittedData(dataArray);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    } else {
+      setErrors(["First name is required!"]);
+    }
+  }
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {/* conditionally render error messages */}
+      {errors.length > 0
+        ? errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))
+        : null}
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
 
